f4291471932a1dedcadc00517abf7812f0b697ae
VictorLMoura <victorlaperutademoura@hotmail.com>
2018-04-09 16:35:33 -0300

    Testes Fugir Leite

---
+++ ros/arquivos_python/scripts/detect_feature.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+
+'''Imports for ros'''
+import rospy
+import tf
+from geometry_msgs.msg import Twist, Vector3, Pose
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+import smach
+import smach_ros
+
+import matplotlib.cm as cm
+import math
+from math import pi
+from __future__ import print_function
+import sys
+    
+    
+def matches(frame)
+    imagem = 'leite.jpg'
+
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame = cv2.blur(frame,(5,5)) # Tira ruido
+
+    centro = (frame.shape[0]//2, frame.shape[1]//2)
+
+
+
+    MIN_MATCH_COUNT = 20
+    img1 = cv2.imread(imagem,0)          # Imagem a procurar
+    img2 = frame 
+
+    # Initiate SIFT detector
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT in each image
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    # Configura o algoritmo de casamento de features
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    # Tenta fazer a melhor comparacao usando o algoritmo
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+        maior_contorno = None
+    maior_contorno_area = 0
+
+    for cnt in contornos:
+	    area = cv2.contourArea(cnt)
+	    if area > maior_contorno_area:
+	        maior_contorno = cnt
+	        maior_contorno_area = area
+
+    ''' PARTE DA MÉDIA E AREA'''
+    '''Pega cada item Y da lista maior_contorno e salva em outra lista pra
+    depois pegar o maximo e minimo'''
+    array_y=[]
+    distancia_cm = ""
+    # Encontramos o centro do contorno fazendo a média de todos seus pontos.
+    if not maior_contorno is None : #Para não dar erro caso não ache o maior_contorno
+        # print(maior_contorno)
+        maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
+        media = maior_contorno.mean(axis=0)
+        media = media.astype(np.int32)
+        # print(media) #Tupla onde o primeiro inteiro é a posição media na
+                     #horizontal e o segunodo é na vertical
+                     #1o varia de 0(esquerda) a 1000(direita)
+    else:
+         media = (0, 0)
+    
+    centro = (frame.shape[0]//2, frame.shape[1]//2)
+
+
+    return media, centro, good
--- ros/arquivos_python/scripts/escape_leite.py
+++ ros/arquivos_python/scripts/escape_leite.py
@@ -1,42 +1,41 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = ["Rachel P. B. Moraes", "Igor Montagner", "Fabio Miranda"]
+
+
 import rospy
+import numpy as np
 import tf
 import math
+import cv2
+import time
 from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
-import cormodule
-
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-import time
-import matplotlib.cm as cm
-from math import pi
-from __future__ import print_function
-import sys
-import math
+import detect_feature
 
 
+bridge = CvBridge()
+
+cv_image = None
 media = []
 centro = []
-atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
+atraso = 3E9
+
 area = 0.0 # Variavel com a area do maior contorno
-check_delay = False # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. Descarta imagens que chegam atrasadas demais
+
+check_delay = True # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. Descarta imagens que chegam atrasadas demais
 
 
-imagem = 'leite.jpg'
-leite = cv2.imread(imagem, cv2.IMREAD_GRAYSCALE)
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-
-def roda_todo_frame(imagem):  ### DESCOBRIR SE ESSA FUNÇÃO É NECESSÁRIA
-	print("frame")
+def roda_todo_frame(imagem):
+	# print("frame")
 	global cv_image
 	global media
 	global centro
+    global good_matches 
 
 	now = rospy.get_rostime()
 	imgtime = imagem.header.stamp
@@ -45,34 +44,33 @@ def roda_todo_frame(imagem):  ### DESCOBRIR SE ESSA FUNÇÃO É NECESSÁRIA
 	print("delay ", "{:.3f}".format(delay/1.0E9))
 	if delay > atraso and check_delay==True:
 		print("Descartando por causa do delay do frame:", delay)
-		return 
+		return
 	try:
 		antes = time.clock()
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-		media, centro, area =  detect_feature.matches(cv_image)
+		media, centro, good_matches =  matches(cv_image)
 		depois = time.clock()
+		# cv2.putText(cv_image, "Distancia: {0}".format(distancia_cm), (10,300),cv2.FONT_HERSHEY_SIMPLEX,1.5,color=(255,255,255))
 		cv2.imshow("Camera", cv_image)
 	except CvBridgeError as e:
 		print('ex', e)
 
-if __name__=="__main__":
-    '''
-    rospy.init_node(" ")
-    '''
 
-    # Para usar a Raspberry Pi
+
+if __name__=="__main__":
+
+	rospy.init_node("escape_leite")
+
+	# Para usar a Raspberry Pi
 	topico_raspberry_camera = "/raspicam_node/image/compressed"
-	# Para usar a webcam 
+	# Para usar a webcam
 	topico_webcam = "/cv_camera/image_raw/compressed"
 
 
-    topico_imagem = topico_raspberry_camera
+	topico_imagem = topico_raspberry_camera
 
-    recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24) #ONDE USAR?#
-
+	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
 	print("Usando ", topico_imagem)
-    
-print("Usando ", topico_imagem)
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
@@ -80,7 +78,7 @@ print("Usando ", topico_imagem)
 
 		while not rospy.is_shutdown():
 			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-			if len(media) != 0 and len(centro) != 0:
+			if len(media) != 0 and len(centro) != 0 and len(good_matches)>20 :
 				dif_x = media[0]-centro[0]
 				dif_y = media[1]-centro[1]
 				cv2.putText(cv_image, "dif_x: {0}".format(dif_x), (10,200),cv2.FONT_HERSHEY_SIMPLEX,1.5,color=(255,255,255))
@@ -100,5 +98,3 @@ print("Usando ", topico_imagem)
 
 	except rospy.ROSInterruptException:
 	    print("Ocorreu uma exceção com o rospy")
-
-        
