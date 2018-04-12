c7a09612b724de449d2e882b5aa03be7751510d7
WarlenRodrigues <warlencrf@al.insper.edu.br>
2018-04-12 16:10:45 -0300

    le_scan funcionando!

--- ros/arquivos_python/scripts/le_scan.py
+++ ros/arquivos_python/scripts/le_scan.py
@@ -2,67 +2,50 @@
 # -*- coding:utf-8 -*-
 
 import rospy
-
 import numpy as np
-
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 
-
 def scaneou(dado):
 	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
-	print("Leituras:")
+	lista_valores = np.array(dado.ranges).round(decimals = 2)
 	iteracoes = 0
-	distancia = dado.range_min
-	atencao = 0.15
-	for i in len(dado.ranges):
-		if dado.ranges[i] == dado.range_min:
-			if dado.ranges[i] != 0 and 0 < i < 90:
-				velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-				velocidade_saida.publish(velocidade)
+	atencao = 0.3
 
-			elif dado.ranges[i] != 0 and 90 < i < 180:
-				velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-				velocidade_saida.publish(velocidade)
+	for i in range(len(lista_valores)):
+		if lista_valores[i] == 0:
+			lista_valores[i] = 33
+		else:
+			iteracoes += 1
 
-			elif dado.ranges[i] != 0 and 180 < i < 270:
-				velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-				velocidade_saida.publish(velocidade)
+	velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+	velocidade_saida.publish(velocidade)
 
-			elif dado.ranges[i] != 0 and 270 < i < 360:
-				velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-				velocidade_saida.publish(velocidade)
+	distancia = min(lista_valores)
+	print("Distancia do Objeto mais Proximo:", distancia)
+	if distancia < atencao:
+		for i in range(len(lista_valores)):
+			if 0 < i < 90 and lista_valores[i] == distancia:
+				velocidade = Twist(Vector3(-2, 0, 0), Vector3(0, 0, -2))
+				print("VAI BATER - NW")
 
-			else:
-				iteracoes += 1
-		print (np.array(dado.ranges).round(decimals=3))
-#		else:
-#			iteracoes += 1
-		# elif 0 < i < 90:
-		# 	if 0 < dado.ranges[i] < atencao:
-		# elif 90 < i < 180:
-		# 	if 0 < dado.ranges[i] < atencao:
-		# 		velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-		# 		velocidade_saida.publish(velocidade)
-		# elif 180 < i < 270:
-		# 	if 0 < dado.ranges[i] < atencao:
-		# 		velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-		# 		velocidade_saida.publish(velocidade)
-		# elif 270 < i < 360:
-		# 	if 0 < dado.ranges[i] < atencao:
-		# 		velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-		# 		velocidade_saida.publish(velocidade)
-		# else:
-		# 	iteracoes += 1
-#	print(np.array(dado.ranges).round(decimals=3))
+			elif 90 < i < 180 and lista_valores[i] == distancia:
+				velocidade = Twist(Vector3(2, 0, 0), Vector3(0, 0, -2))
+				print("VAI BATER - SW")
 
+			elif 180 < i < 270 and lista_valores[i] == distancia:
+				velocidade = Twist(Vector3(2, 0, 0), Vector3(0, 0, 2))
+				print("VAI BATER - SE")
 
+			elif 270 < i < 360 and lista_valores[i] == distancia:
+				velocidade = Twist(Vector3(-2, 0, 0), Vector3(0, 0, 2))
+				print("VAI BATER - NE")
 
+		velocidade_saida.publish(velocidade)
 
-#			velocidade = Twist(Vector3(0, 0, 2), Vector3(0, 0, 2))
-#			velocidade_saida.publish(velocidade)
-#	else:
-#		iteracoes += 1
+	else:
+		print("OK")
+		iteracoes += 1
 
 if __name__=="__main__":
 
@@ -74,7 +57,4 @@ if __name__=="__main__":
 
 
 	while not rospy.is_shutdown():
-		#print("Oeee")
-		velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
-		velocidade_saida.publish(velocidade)
-		rospy.sleep(2)
+		rospy.sleep(1)
